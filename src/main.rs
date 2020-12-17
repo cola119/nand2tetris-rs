bbc5854de23d1f40b4f9d998a9a94afc09c3426d
cola119 <k.cola119@ezweb.ne.jp>
2020-12-17 22:04:33 +0900

    and, not, or, xor

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use crate::Bit::{O, I};
+use crate::Bit::{I, O};
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Copy, Clone)]
 pub enum Bit {
     O,
     I,
@@ -19,14 +19,30 @@ pub fn nand(a: Bit, b: Bit) -> Bit {
     }
 }
 
+pub fn not(a: Bit) -> Bit {
+    nand(a, I)
+}
+
+pub fn and(a: Bit, b: Bit) -> Bit {
+    not(nand(a, b))
+}
+
+pub fn or(a: Bit, b: Bit) -> Bit {
+    nand(not(a), not(b))
+}
+
+pub fn xor(a: Bit, b: Bit) -> Bit {
+    and(or(a, b), nand(a, b))
+}
+
 fn main() {
     println!("Hello, world!");
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{nand};
-    use super::Bit::{O, I};
+    use super::Bit::{I, O};
+    use super::{and, nand, not, or, xor};
 
     #[test]
     fn for_nand() {
@@ -34,5 +50,35 @@ mod tests {
         assert_eq!(nand(I, O), I);
         assert_eq!(nand(O, I), I);
         assert_eq!(nand(I, I), O);
+    }
+
+    #[test]
+    fn for_not() {
+        assert_eq!(not(O), I);
+        assert_eq!(not(I), O);
+    }
+
+    #[test]
+    fn for_and() {
+        assert_eq!(and(O, O), O);
+        assert_eq!(and(I, O), O);
+        assert_eq!(and(O, I), O);
+        assert_eq!(and(I, I), I);
+    }
+
+    #[test]
+    fn for_or() {
+        assert_eq!(or(O, O), O);
+        assert_eq!(or(I, O), I);
+        assert_eq!(or(O, I), I);
+        assert_eq!(or(I, I), I);
+    }
+
+    #[test]
+    fn for_xor() {
+        assert_eq!(xor(O, O), O);
+        assert_eq!(xor(I, O), I);
+        assert_eq!(xor(O, I), I);
+        assert_eq!(xor(I, I), O);
     }
 }
