89822e5e76e3f339cf6b60582013ec80bee849c7
cola119 <k.cola119@ezweb.ne.jp>
2020-12-17 21:36:00 +0900

    nand

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use crate::Bit::{O, I};
+
+#[derive(Debug, PartialEq)]
+pub enum Bit {
+    O,
+    I,
+}
+
+pub fn nand(a: Bit, b: Bit) -> Bit {
+    match a {
+        O => match b {
+            O => I,
+            I => I,
+        },
+        I => match b {
+            O => I,
+            I => O,
+        },
+    }
+}
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{nand};
+    use super::Bit::{O, I};
+
+    #[test]
+    fn for_nand() {
+        assert_eq!(nand(O, O), I);
+        assert_eq!(nand(I, O), I);
+        assert_eq!(nand(O, I), I);
+        assert_eq!(nand(I, I), O);
+    }
 }
