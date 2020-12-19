d57cf37982003939612d112c02ae89b4c2a803a9
cola119 <k.cola119@ezweb.ne.jp>
2020-12-19 13:46:33 +0900

    16

--- src/main.rs
+++ src/main.rs
@@ -78,6 +78,69 @@ pub fn not16(bits: Word) -> Word {
     ])
 }
 
+pub fn and16(a: Word, b: Word) -> Word {
+    Word([
+        and(a[0], b[0]),
+        and(a[1], b[1]),
+        and(a[2], b[2]),
+        and(a[3], b[3]),
+        and(a[4], b[4]),
+        and(a[5], b[5]),
+        and(a[6], b[6]),
+        and(a[7], b[7]),
+        and(a[8], b[8]),
+        and(a[9], b[9]),
+        and(a[10], b[10]),
+        and(a[11], b[11]),
+        and(a[12], b[12]),
+        and(a[13], b[13]),
+        and(a[14], b[14]),
+        and(a[15], b[15]),
+    ])
+}
+
+pub fn or16(a: Word, b: Word) -> Word {
+    Word([
+        or(a[0], b[0]),
+        or(a[1], b[1]),
+        or(a[2], b[2]),
+        or(a[3], b[3]),
+        or(a[4], b[4]),
+        or(a[5], b[5]),
+        or(a[6], b[6]),
+        or(a[7], b[7]),
+        or(a[8], b[8]),
+        or(a[9], b[9]),
+        or(a[10], b[10]),
+        or(a[11], b[11]),
+        or(a[12], b[12]),
+        or(a[13], b[13]),
+        or(a[14], b[14]),
+        or(a[15], b[15]),
+    ])
+}
+
+pub fn mux16(a: Word, b: Word, sel: Bit) -> Word {
+    Word([
+        mux(a[0], b[0], sel),
+        mux(a[1], b[1], sel),
+        mux(a[2], b[2], sel),
+        mux(a[3], b[3], sel),
+        mux(a[4], b[4], sel),
+        mux(a[5], b[5], sel),
+        mux(a[6], b[6], sel),
+        mux(a[7], b[7], sel),
+        mux(a[8], b[8], sel),
+        mux(a[9], b[9], sel),
+        mux(a[10], b[10], sel),
+        mux(a[11], b[11], sel),
+        mux(a[12], b[12], sel),
+        mux(a[13], b[13], sel),
+        mux(a[14], b[14], sel),
+        mux(a[15], b[15], sel),
+    ])
+}
+
 fn main() {
     println!("Hello, world!");
 }
@@ -85,7 +148,7 @@ fn main() {
 #[cfg(test)]
 mod tests {
     use super::Bit::{I, O};
-    use super::{and, dmux, mux, nand, not, not16, or, xor, Word};
+    use super::{and, and16, dmux, mux, mux16, nand, not, not16, or, or16, xor, Word};
 
     #[test]
     fn for_nand() {
@@ -156,6 +219,78 @@ mod tests {
         assert_eq!(
             not16(Word([I, I, I, O, I, O, I, O, I, O, I, O, I, I, I, O])),
             Word([O, O, O, I, O, I, O, I, O, I, O, I, O, O, O, I])
+        );
+    }
+
+    #[test]
+    fn for_and16() {
+        assert_eq!(and16(Word([O; 16]), Word([O; 16])), Word([O; 16]));
+        assert_eq!(and16(Word([I; 16]), Word([O; 16])), Word([O; 16]));
+        assert_eq!(and16(Word([O; 16]), Word([I; 16])), Word([O; 16]));
+        assert_eq!(and16(Word([I; 16]), Word([I; 16])), Word([I; 16]));
+        assert_eq!(
+            and16(
+                Word([I, O, I, O, I, O, I, O, I, O, I, O, I, O, I, O]),
+                Word([I, O, I, O, I, O, I, O, I, O, I, O, I, O, I, O])
+            ),
+            Word([I, O, I, O, I, O, I, O, I, O, I, O, I, O, I, O])
+        );
+        assert_eq!(
+            and16(
+                Word([I, O, I, O, I, O, I, O, I, O, I, O, I, O, I, O]),
+                Word([O, I, O, I, O, I, O, I, O, I, O, I, O, I, O, I])
+            ),
+            Word([O; 16])
+        );
+    }
+
+    #[test]
+    fn for_or16() {
+        assert_eq!(or16(Word([O; 16]), Word([O; 16])), Word([O; 16]));
+        assert_eq!(or16(Word([I; 16]), Word([O; 16])), Word([I; 16]));
+        assert_eq!(or16(Word([O; 16]), Word([I; 16])), Word([I; 16]));
+        assert_eq!(or16(Word([I; 16]), Word([I; 16])), Word([I; 16]));
+        assert_eq!(
+            or16(
+                Word([I, O, I, O, I, O, I, O, I, O, I, O, I, O, I, O]),
+                Word([I, O, I, O, I, O, I, O, I, O, I, O, I, O, I, O])
+            ),
+            Word([I, O, I, O, I, O, I, O, I, O, I, O, I, O, I, O])
+        );
+        assert_eq!(
+            or16(
+                Word([I, O, I, O, I, O, I, O, I, O, I, O, I, O, I, O]),
+                Word([O, I, O, I, O, I, O, I, O, I, O, I, O, I, O, I])
+            ),
+            Word([I; 16])
+        );
+    }
+
+    #[test]
+    fn for_mux16() {
+        assert_eq!(mux16(Word([O; 16]), Word([O; 16]), O), Word([O; 16]));
+        assert_eq!(mux16(Word([O; 16]), Word([I; 16]), O), Word([O; 16]));
+        assert_eq!(mux16(Word([I; 16]), Word([O; 16]), O), Word([I; 16]));
+        assert_eq!(mux16(Word([I; 16]), Word([I; 16]), O), Word([I; 16]));
+        assert_eq!(mux16(Word([O; 16]), Word([O; 16]), I), Word([O; 16]));
+        assert_eq!(mux16(Word([I; 16]), Word([O; 16]), I), Word([O; 16]));
+        assert_eq!(mux16(Word([O; 16]), Word([I; 16]), I), Word([I; 16]));
+        assert_eq!(mux16(Word([I; 16]), Word([I; 16]), I), Word([I; 16]));
+        assert_eq!(
+            mux16(
+                Word([I, O, I, O, I, O, I, O, I, O, I, O, I, O, I, O]),
+                Word([O, I, O, I, O, I, O, I, O, I, O, I, O, I, O, I]),
+                O
+            ),
+            Word([I, O, I, O, I, O, I, O, I, O, I, O, I, O, I, O])
+        );
+        assert_eq!(
+            mux16(
+                Word([I, O, I, O, I, O, I, O, I, O, I, O, I, O, I, O]),
+                Word([O, I, O, I, O, I, O, I, O, I, O, I, O, I, O, I]),
+                I
+            ),
+            Word([O, I, O, I, O, I, O, I, O, I, O, I, O, I, O, I])
         );
     }
 }
