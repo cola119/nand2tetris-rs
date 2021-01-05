bb9819dd3fae48ebae22033e09c168c0f2f507a8
cola119 <k.cola119@ezweb.ne.jp>
2021-01-05 21:56:34 +0900

    pong

--- assembler/src/main.rs
+++ assembler/src/main.rs
@@ -1,8 +1,17 @@
+use std::fs::File;
+use std::io::Write;
+
+use parser::Parser;
+
 mod code;
 mod parser;
 mod symbol_table;
 mod util;
 
 fn main() {
-    println!("Hello, world!");
+    let mut parser = Parser::new();
+    let result = parser.run("src/programs/Pong.asm");
+
+    let mut file = File::create("src/programs/pong.txt").unwrap();
+    file.write_all(result.to_string().as_bytes()).unwrap();
 }
